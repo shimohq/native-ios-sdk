cce31d7c31d7cc4bb41e4b8ff1fd56d61a6d445a
钟加仁 <zhongjiaren@shimo.im>
2022-05-23 17:11:54 +0800

    fix: 修复 url 可能重复加载问题

--- NativeSDK/SHMWebView/SHMWebView.m
+++ NativeSDK/SHMWebView/SHMWebView.m
@@ -447,7 +447,7 @@ NSString *const SHMWebViewVersion = @"1.35";
 
 - (void)setUrl:(NSURL *)url {
     _url = url;
-    if (_webview && _url) {
+    if (_url && _webview && _webview.superview) {
         [_webview loadRequest:[NSURLRequest requestWithURL:_url]];
     }
 }
