201f39da220f7fcf29557d1031b928a2b437d8e5
钟加仁 <zhongjiaren@shimo.im>
2022-05-23 16:38:32 +0800

    feat: 去掉 applicationNameForUserAgent 属性，替换为 appID lang dir

--- NativeSDK/SHMWebView/SHMWebView.h
+++ NativeSDK/SHMWebView/SHMWebView.h
@@ -15,11 +15,13 @@
 @protocol SHMWebViewDelegate <NSObject>
 
 /// 设置标题
+///
 /// @param webview WebView 实例
-/// @param title 标题，为 nil 时表示去掉 JS 设置的标题，还原默认标题
+/// @param title 标题，为 nil 时表示 WebView 没有设置标题，使用自定义的默认标题
 - (void)webview:(nonnull SHMWebView *)webview setNavigatorTitle:(nullable NSString *)title;
 
-/// 根据约定的数据设置导航条按钮
+/// 设置导航条按钮
+///
 /// @param webview WebView 实例
 /// @param buttons 按钮数据
 - (void)webview:(nonnull SHMWebView *)webview setNavigatorButtons:(nonnull NSArray<SHMWebViewNavigatorButton *> *)buttons;
@@ -27,12 +29,13 @@
 @optional
 
 /// 设置返回按钮是否可用
+///
 /// 如果返回按钮常驻就不需要实现该代理
 /// @param webview WebView 实例
-/// @param backButtonEnable 返回按钮是否可用。
-/// YES: 返回按钮可用，点击返回按钮会触发 WebView 的返回事件
+/// @param backButtonEnabled 返回按钮是否可用。
+/// YES: 返回按钮可用，点击返回按钮会触发 WebView 的返回事件。
 /// NO: 返回按钮不可用，点击返回按钮不会触发 WebView 的返回事件
-- (void)webview:(nonnull SHMWebView *)webview setBackButtonEnable:(BOOL)backButtonEnable;
+- (void)webview:(nonnull SHMWebView *)webview setBackButtonEnabled:(BOOL)backButtonEnabled;
 
 @end
 
@@ -44,39 +47,58 @@
 /// 当前石墨部署环境域名
 @property (nonnull, nonatomic, copy) NSString *host;
 
+/// 当前 App 的 ID
+///
+/// Default: NativeSDK-1
+@property (nonnull, nonatomic, copy) NSString *appID;
+
+/// 当前 App 语言
+///
+/// Default: zh-CN
+@property (nonnull, nonatomic, copy) NSString *lang;
+
+/// 当前 App 布局方向
+///
+/// Default: ltr
+@property (nonnull, nonatomic, copy) NSString *dir;
+
 /// WebView 配置
+///
 /// 一般情况不需要赋值，会自动创建新的
 @property (nullable, nonatomic, copy) WKWebViewConfiguration *configuration;
-
-/// WKWebView 默认 user agent 添加后缀
-/// Default: @"SMWV/1.35  (HWMT-730; lang: zh-CN; dir: ltr)"
-/// 根据实际情况填充 lang 和 dir 字段 
-@property (nullable, nonatomic, copy) NSString *applicationNameForUserAgent;
 
 /// 使用 SHMWebView 需要实现的代理
 @property (nullable, nonatomic, weak) id<SHMWebViewDelegate> delegate;
 
 /// WKWebView 的 WKNavigationDelegate
+///
 /// 拦截跳转时用
 @property (nullable, nonatomic, weak) id <WKNavigationDelegate> navigationDelegate;
 
 /// WKWebView 的 WKUIDelegate
+///
 /// 自定义 alert、confirm、promit 和拦截新窗口打开时用
 @property (nullable, nonatomic, weak) id <WKUIDelegate> UIDelegate;
 
 /// 返回按钮是否可用
+///
 /// 默认: NO
-@property (nonatomic, assign, readonly) BOOL backButtonEnable;
+@property (nonatomic, assign, readonly) BOOL backButtonEnabled;
 
-/// didMoveToWindow 的时候自动创建
+/// WKWebView 实例
+///
+/// didMoveToWindow 的时候自动创建，
 /// 尽量不要直接操作 webview，因为无法保证 webview 是否已经创建好。
 @property (nonnull, nonatomic, strong, readonly) WKWebView *webview;
 
 /// 创建并添加 WKWebView 到 SHMWebView
-/// didMoveToWindow 的时候会自动调用，除非需要提前获取 WKWebView，否则不要调用该方法。
+///
+/// didMoveToWindow 的时候会自动调用，一般情况不需要主动调用该方法，
+/// 除非需要在所有属性配置好后立即获取 WKWebView。
 - (nonnull WKWebView *)createAndSetWebView;
 
 /// 用户点击原生返回按钮时，调用该方法判断是否执行原生返回。
+///
 /// 返回值 nativeGoBack 为 YES 时执行的原生返回，为 NO 表示 Web 内已执行返回不需要原生返回。
 /// @param callback JS 回调
 - (void)goBackWithCallback:(void (^__nullable)(BOOL nativeGoBack))callback;
